35e4370f222c4f8366b4d8fdf005bdb498ed4354
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-18 08:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
+function mincost(arr){
+
+	if (ropes.length <= 1) return 0;
+
+    // Sort ropes initially
+    ropes.sort((a, b) => a - b);
+
+    let totalCost = 0;
+
+    while (ropes.length > 1) {
+        // Take two smallest ropes
+        let first = ropes.shift();
+        let second = ropes.shift();
+
+        let cost = first + second;
+        totalCost += cost;
+
+        // Insert back and keep array sorted
+        let index = ropes.findIndex(r => r > cost);
+        if (index === -1) ropes.push(cost);
+        else ropes.splice(index, 0, cost);
+    }
+
+
   
 }
 
